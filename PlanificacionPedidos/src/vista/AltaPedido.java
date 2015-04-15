93f95ae4126282e5bdd2bb1cee4be9bd9990120d
kronopi0 <edugsara@gmail.com>
2015-04-15 13:17:02 -0300

    Subo las ventanas que hizo Juan

---
+++ PlanificacionPedidos/src/vista/AltaPedido.java
@@ -0,0 +1,161 @@
+package vista;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+
+public class AltaPedido extends javax.swing.JFrame {
+	private JLabel jLabelCuit;
+	private JButton jButtonBuscarCliente;
+	private JLabel jLabelNombreCliente;
+	private JTextField jTextFieldCodigoPedido;
+	private JLabel jLabelFechaDeEntrega;
+	private JButton jButtonSalir;
+	private JButton jButtonConfirmar;
+	private JSeparator jSeparator2;
+	private JTextField jTextFieldDescripcion;
+	private JTextField jTextFieldPeriodicidad;
+	private JTextField jTextFieldFechaDeEntrega;
+	private JLabel jLabelDescripcion;
+	private JLabel jLabelPeriodicidad;
+	private JLabel jLabelCodigoPedido;
+	private JSeparator jSeparator1;
+	private JTextField jTextFieldNombreCliente;
+	private JTextField jTextFieldCuit;
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				AltaPedido inst = new AltaPedido();
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public AltaPedido() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			getContentPane().setLayout(null);
+			this.setTitle("Alta Pedido");
+			{
+				jLabelCuit = new JLabel();
+				getContentPane().add(jLabelCuit);
+				jLabelCuit.setText("Cuit Cliente :");
+				jLabelCuit.setBounds(12, 19, 70, 16);
+			}
+			{
+				jTextFieldCuit = new JTextField();
+				getContentPane().add(jTextFieldCuit);
+				jTextFieldCuit.setBounds(94, 16, 102, 23);
+			}
+			{
+				jButtonBuscarCliente = new JButton();
+				getContentPane().add(jButtonBuscarCliente);
+				jButtonBuscarCliente.setText("Buscar Cliente");
+				jButtonBuscarCliente.setBounds(228, 16, 130, 23);
+			}
+			{
+				jLabelNombreCliente = new JLabel();
+				getContentPane().add(jLabelNombreCliente);
+				jLabelNombreCliente.setText("Nombre :");
+				jLabelNombreCliente.setBounds(12, 53, 62, 16);
+			}
+			{
+				jTextFieldNombreCliente = new JTextField();
+				getContentPane().add(jTextFieldNombreCliente);
+				jTextFieldNombreCliente.setText("No Existe el Cliente");
+				jTextFieldNombreCliente.setBounds(74, 51, 284, 23);
+			}
+			{
+				jSeparator1 = new JSeparator();
+				getContentPane().add(jSeparator1);
+				jSeparator1.setBounds(-10, 89, 418, 10);
+			}
+			{
+				jLabelCodigoPedido = new JLabel();
+				getContentPane().add(jLabelCodigoPedido);
+				jLabelCodigoPedido.setText("Código de Pedido :");
+				jLabelCodigoPedido.setBounds(14, 105, 110, 16);
+			}
+			{
+				jTextFieldCodigoPedido = new JTextField();
+				getContentPane().add(jTextFieldCodigoPedido);
+				jTextFieldCodigoPedido.setBounds(136, 102, 128, 23);
+			}
+			{
+				jLabelFechaDeEntrega = new JLabel();
+				getContentPane().add(jLabelFechaDeEntrega);
+				jLabelFechaDeEntrega.setText("Fecha de Entrega :");
+				jLabelFechaDeEntrega.setBounds(14, 140, 110, 16);
+			}
+			{
+				jLabelPeriodicidad = new JLabel();
+				getContentPane().add(jLabelPeriodicidad);
+				jLabelPeriodicidad.setText("Periodicidad :");
+				jLabelPeriodicidad.setBounds(14, 176, 110, 16);
+			}
+			{
+				jLabelDescripcion = new JLabel();
+				getContentPane().add(jLabelDescripcion);
+				jLabelDescripcion.setText("Descripción :");
+				jLabelDescripcion.setBounds(14, 212, 110, 16);
+			}
+			{
+				jTextFieldFechaDeEntrega = new JTextField();
+				getContentPane().add(jTextFieldFechaDeEntrega);
+				jTextFieldFechaDeEntrega.setText("dd/mm/aaaa");
+				jTextFieldFechaDeEntrega.setBounds(136, 137, 128, 23);
+			}
+			{
+				jTextFieldPeriodicidad = new JTextField();
+				getContentPane().add(jTextFieldPeriodicidad);
+				jTextFieldPeriodicidad.setBounds(136, 173, 128, 23);
+			}
+			{
+				jTextFieldDescripcion = new JTextField();
+				getContentPane().add(jTextFieldDescripcion);
+				jTextFieldDescripcion.setBounds(136, 209, 222, 74);
+			}
+			{
+				jSeparator2 = new JSeparator();
+				getContentPane().add(jSeparator2);
+				jSeparator2.setBounds(-10, 295, 400, 10);
+			}
+			{
+				jButtonConfirmar = new JButton();
+				getContentPane().add(jButtonConfirmar);
+				jButtonConfirmar.setText("Confirmar");
+				jButtonConfirmar.setBounds(136, 311, 98, 23);
+			}
+			{
+				jButtonSalir = new JButton();
+				getContentPane().add(jButtonSalir);
+				jButtonSalir.setText("Salir");
+				jButtonSalir.setBounds(290, 311, 68, 23);
+				jButtonSalir.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent evt) 
+					{
+						dispose();
+					}
+				});
+			}
+			pack();
+			this.setSize(400, 386);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- PlanificacionPedidos/src/vista/MenuPrincipal.java
+++ PlanificacionPedidos/src/vista/MenuPrincipal.java
@@ -1,188 +1,126 @@
 package vista;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.AbstractAction;
-import javax.swing.AbstractButton;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JTabbedPane;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.WindowConstants;
 import javax.swing.SwingUtilities;
+import entities.Pedido;
 
-import controlador.Sistema;
-
-/**
- * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
- * Builder, which is free for non-commercial use. If Jigloo is being used
- * commercially (ie, by a corporation, company or business for any purpose
- * whatever) then you should purchase a license for each developer using Jigloo.
- * Please visit www.cloudgarden.com for details. Use of Jigloo implies
- * acceptance of these licensing terms. A COMMERCIAL LICENSE HAS NOT BEEN
- * PURCHASED FOR THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED LEGALLY FOR
- * ANY CORPORATE OR COMMERCIAL PURPOSE.
- */
 public class MenuPrincipal extends javax.swing.JFrame {
-	private JMenuBar Menu;
-	private JMenuItem ModifCliente;
-	private JMenuItem NuevoCliente;
-	private JMenuItem SalirItem;
-	private JMenuItem ConsultarDeudas;
-	private JMenu Info;
-	private JMenu Cliente;
-	private JMenu File;
-	private JTabbedPane tabbedPane;
-	private AbstractAction Alta;
-	private JMenuItem Integrantes;
-	private static Sistema sist;
+	private JButton jButtonAlta;
+	private JButton jButtonFinalizar;
+	private JButton jButtonSalir;
+	private JSeparator jSeparator2;
+	private JButton jButtonReporte;
+	private JLabel jLabel1;
+	private JSeparator jSeparator1;
+	private JLabel jLabelPedidos;
+	private JButton jButtonProgramar;
 
-	/**
-	 * Auto-generated main method to display this JFrame
-	 */
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				MenuPrincipal inst = new MenuPrincipal();
 				inst.setLocationRelativeTo(null);
 				inst.setVisible(true);
-				sist = Sistema.getInstancia();
-
 			}
 		});
 	}
-
+	
 	public MenuPrincipal() {
 		super();
 		initGUI();
 	}
-
+	
 	private void initGUI() {
 		try {
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			getContentPane().setLayout(null);
+			this.setTitle("Menu Principal");
+			this.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) 
+				  {
+				     System.exit(0);
+				  }
+			});
 			{
-				tabbedPane = new JTabbedPane();
-				getContentPane().add(tabbedPane, BorderLayout.CENTER);
+				jButtonAlta = new JButton();
+				getContentPane().add(jButtonAlta);
+				jButtonAlta.setText("Alta");
+				jButtonAlta.setBounds(90, 24, 84, 29);
+				jButtonAlta.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent evt){
+						AltaPedido app = new AltaPedido();
+						app.setVisible(true);
+					}
+				});
 			}
-			setDefaultLookAndFeelDecorated(true);
 			{
-				Menu = new JMenuBar();
-				setJMenuBar(Menu);
-				{
-					File = new JMenu();
-					Menu.add(File);
-					File.setText("Archivo");
-					{
-						SalirItem = new JMenuItem();
-						File.add(SalirItem);
-						SalirItem.setText("Salir");
-						SalirItem.addActionListener(new ActionListener() {
-
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								dispose();
-							}
-						});
-					}
-				}
-				{
-					Cliente = new JMenu();
-					Menu.add(Cliente);
-					Cliente.setText("Gestionar Pedidos");
-					{
-						NuevoCliente = new JMenuItem();
-						Cliente.add(NuevoCliente);
-						NuevoCliente.setText("Programar Pedidos");
-						NuevoCliente.addActionListener(new ActionListener() {
-
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								int p = -1;
-								for (int i = 0; i < tabbedPane.getTabCount(); i++) {
-									String s = tabbedPane.getComponentAt(i).getClass().toString();
-									System.out.print(s);
-									if (s.endsWith("Programar Pedidos")) {
-										p = i;
-									}
-								}
-								if (p > -1) {
-									tabbedPane.setSelectedIndex(p);
-								} else {
-									Component c;
-									c = new ProgramarPedidos();
-									tabbedPane.add("Programar Pedidos", c);
-									int ip = tabbedPane.indexOfComponent(c);
-									tabbedPane.setSelectedIndex(ip);
-								}
-							}
-						});
-					}
-					{
-						ModifCliente = new JMenuItem();
-						Cliente.add(ModifCliente);
-						ModifCliente.setText("Listar Pedidos Programados");
-						ModifCliente.addActionListener(new ActionListener() {
-
-							@Override
-							public void actionPerformed(ActionEvent arg0) {
-								int p = -1;
-								for (int i = 0; i < tabbedPane.getTabCount(); i++) {
-									String s = tabbedPane.getComponentAt(i).getClass().toString();
-									if (s.endsWith("ModificarCliente")) {
-										p = i;
-									}
-								}
-								if (p > -1) {
-									tabbedPane.setSelectedIndex(p);
-								} else {
-									Component c;
-									c = new ListarPedidosProgramados(tabbedPane, sist);
-									tabbedPane.add("Modificar Cliente", c);
-									int ip = tabbedPane.indexOfComponent(c);
-									tabbedPane.setSelectedIndex(ip);
-								}
-							}
-						});
-					}
-
-				}
-
-				{
-					Info = new JMenu();
-					Menu.add(Info);
-					Info.setText("Info");
-					{
-						Integrantes = new JMenuItem();
-						Info.add(Integrantes);
-						Integrantes.setText("Integrantes");
-						Integrantes.addActionListener(new ActionListener() {
-
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								JOptionPane.showMessageDialog(null, "Integrantes:\n    Manuel Alen Freire\n    Juan Manuel Martinez Godoy\n    Eduardo Sara");
-							}
-						});
-					}
-				}
+				jButtonProgramar = new JButton();
+				getContentPane().add(jButtonProgramar);
+				jButtonProgramar.setText("Programar");
+				jButtonProgramar.setBounds(179, 24, 96, 29);
 			}
-
+			{
+				jButtonFinalizar = new JButton();
+				getContentPane().add(jButtonFinalizar);
+				jButtonFinalizar.setText("Finalizar");
+				jButtonFinalizar.setBounds(280, 24, 84, 29);
+			}
+			{
+				jLabelPedidos = new JLabel();
+				getContentPane().add(jLabelPedidos);
+				jLabelPedidos.setText("Pedidos");
+				jLabelPedidos.setBounds(15, 24, 68, 29);
+			}
+			{
+				jSeparator1 = new JSeparator();
+				getContentPane().add(jSeparator1);
+				jSeparator1.setBounds(0, 78, 384, 10);
+			}
+			{
+				jLabel1 = new JLabel();
+				getContentPane().add(jLabel1);
+				jLabel1.setText("Reportes");
+				jLabel1.setBounds(12, 104, 68, 29);
+			}
+			{
+				jButtonReporte = new JButton();
+				getContentPane().add(jButtonReporte);
+				jButtonReporte.setText("Reporte");
+				jButtonReporte.setBounds(92, 104, 84, 29);
+			}
+			{
+				jSeparator2 = new JSeparator();
+				getContentPane().add(jSeparator2);
+				jSeparator2.setBounds(0, 160, 384, 40);
+			}
+			{
+				jButtonSalir = new JButton();
+				getContentPane().add(jButtonSalir);
+				jButtonSalir.setText("Salir");
+				jButtonSalir.setBounds(279, 166, 84, 29);
+				jButtonSalir.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent evt) 
+					{
+							System.exit(0);
+					}
+				});
+			}
 			pack();
-			this.setSize(650, 450);
+			this.setSize(400, 238);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	public Sistema getSist() {
-		return sist;
-	}
-
-	public void setSist(Sistema sist) {
-		MenuPrincipal.sist = sist;
 	}
 
 }
