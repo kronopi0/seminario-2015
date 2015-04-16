f42dd3f83f3f7ff9340d8b0651cb4b651ebf36d4
deh <deh@manuDesktop>
2015-04-16 00:15:57 -0300

    sql inicio

--- PlanificacionPedidos/sql/carga_inicial.sql
+++ PlanificacionPedidos/sql/carga_inicial.sql
@@ -9,16 +9,37 @@ insert into COMPLEJIDADES_PEDIDOS values ('Intermedia', 1.2)
 insert into COMPLEJIDADES_PEDIDOS values ('Baja', 1)
 
 --CLIENTES
-insert into CLIENTES values (11111, 'Unilever', 'Argentina', '4555-1111', 'Reconquista 415', 'contacto@unilever.com')
-insert into CLIENTES values (22222, 'Danone', 'Argentina', '4555-2222', 'Cabildo 5643', 'contacto@danone.com')
+insert into CLIENTES values (11111, 'ArcosDorados', 'Argentina', '4555-1463', 'Sarasa 415', 'contacto@arcos.com')
+insert into CLIENTES values (44444, 'Matarazzo', 'Argentina', '4554-1111', 'Villa 31', 'contacto@matarazzo.com')
+insert into CLIENTES values (55555, 'UADE', 'Argentina', '4555-1535', 'Italia 2', 'contacto@uade.com')
+insert into CLIENTES values (66666, 'Unilever', 'Argentina', '4754-1111', 'Reconquista 415', 'contacto@unilever.com')
+insert into CLIENTES values (22222, 'Danone', 'Argentina', '4555-2242', 'Cabildo 5643', 'contacto@danone.com')
 insert into CLIENTES values (33333, 'Coca-Cola', 'Argentina', '4555-3333', 'Belgrano 467', 'contacto@coca-cola.com')
 
 --EMPLEADOS
-insert into EMPLEADOS values (126031, 'Manuel', 'Alen')
-insert into EMPLEADOS values (136378, 'Juan Manuel', 'Martinez Godoy')
-insert into EMPLEADOS values (135854, 'Eduardo', 'Sara')
+insert into EMPLEADOS values (12601231, 'Juan', 'Alen')
+insert into EMPLEADOS values (1362378, 'Juan Manuel', 'Godoy')
+insert into EMPLEADOS values (1358354, 'Godoy', 'Martinez')
+insert into EMPLEADOS values (1260341, 'Martinez', 'Alen')
+insert into EMPLEADOS values (13665378, 'Juan Manuel', 'Martinez Godoy')
+insert into EMPLEADOS values (1352854, 'Godoy', 'Juan')
 
---PEDIDOS
-insert into PEDIDOS values ('Pedido 1', 0, '7/4/2015', null, null, 'Pendiente', null, null, null, null)
-insert into PEDIDOS values ('Pedido 2', 0, '9/4/2015', null, null, 'Pendiente', null, null, null, null)
-insert into PEDIDOS values ('Pedido 3', 0, '10/4/2015', null, null, 'Pendiente', null, null, null, null)
+--COMPLEJIDADES_EMPLEADO
+INSERT INTO COMPLEJIDADES_EMPLEADO VALUES (1362378,1)
+INSERT INTO COMPLEJIDADES_EMPLEADO VALUES (1358354,1)
+INSERT INTO COMPLEJIDADES_EMPLEADO VALUES (1358354,2)
+INSERT INTO COMPLEJIDADES_EMPLEADO VALUES (1260341,2)
+INSERT INTO COMPLEJIDADES_EMPLEADO VALUES (1358354,1)
+INSERT INTO COMPLEJIDADES_EMPLEADO VALUES (1358354,3)
+INSERT INTO COMPLEJIDADES_EMPLEADO VALUES (13665378,3)
+INSERT INTO COMPLEJIDADES_EMPLEADO VALUES (1352854,3)
+
+--TIPOS_PEDIDO_EMPLEADO
+INSERT INTO TIPOS_PEDIDO_EMPLEADO VALUES (1362378,2)
+INSERT INTO TIPOS_PEDIDO_EMPLEADO VALUES (1358354,1)
+INSERT INTO TIPOS_PEDIDO_EMPLEADO VALUES (1358354,1)
+INSERT INTO TIPOS_PEDIDO_EMPLEADO VALUES (1260341,2)
+INSERT INTO TIPOS_PEDIDO_EMPLEADO VALUES (1358354,3)
+INSERT INTO TIPOS_PEDIDO_EMPLEADO VALUES (1358354,2)
+INSERT INTO TIPOS_PEDIDO_EMPLEADO VALUES (13665378,1)
+INSERT INTO TIPOS_PEDIDO_EMPLEADO VALUES (1352854,3)
