c7899a45d53dacd58205dab5daa357af77107acf
deh <deh@deh-pc>
2015-06-23 09:35:59 -0300

    correcciones a PedidoProgramar.java, sigue sin andar

--- PlanificacionPedidos/src/vista/PedidoProgramar.java
+++ PlanificacionPedidos/src/vista/PedidoProgramar.java
@@ -1,25 +1,19 @@
 package vista;
 
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
-import java.util.Date;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JOptionPane;
 import javax.swing.JSeparator;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 
-import org.joda.time.DateTime;
-
 import controlador.Sistema;
-import dao.PedidoDAO;
 import dto.ClienteDTO;
 import entities.ComplejidadPedido;
 import entities.Pedido;
@@ -51,32 +45,24 @@ public class PedidoProgramar extends javax.swing.JPanel {
 	private JTabbedPane panel;
 	private JButton jButtonSalir;
 	private PedidoProgramar instancia;
-	private JComboBox<String> comboProgramados;
+	private JComboBox<String> comboPendientes;
 	private List<ClienteDTO> clientes;
 	private JLabel jLabelPedidosPendientes;
 	private Pedido pedido;
 	private List<Pedido> pedidos;
 	private JLabel jLabel1;
 	private JLabel jLabel2;
-	private JComboBox<String> jComboBoxPedidosSinFinalizar;
-	private JButton jButtonFinalizarPedido;
-	private Sistema sistema;
 	private JComboBox<String> comboComplejidad;
 	private JComboBox<String> comboTipo;
 	private JSeparator jSeparator2;
-	private JLabel jLabel4;
-	private JTextField jTextFieldFechaPedidoHasta;
-	private JTextField jTextFieldFechapedidoDesde;
-	private JTextField jTextFieldFechaInicio;
-	private JLabel jLabel3;
-	private JSeparator jSeparator3;
-	private JButton botonDetalles;
 	private TipoPedido tipo;
 	private List<TipoPedido> tipos;
 	private ComplejidadPedido complejidad;
 	private List<ComplejidadPedido> complejidades;
 	private JLabel jLabel5;
 	private JTextField jTextFieldFecha;
+	private JLabel jLabelPeriodicidad;
+	private JTextField jTextFieldPeriodicidad;
 
 	/**
 	 * Auto-generated main method to display this JPanel inside a new JFrame.
@@ -98,35 +84,46 @@ public class PedidoProgramar extends javax.swing.JPanel {
 				jLabelCliente = new JLabel();
 				this.add(jLabelCliente);
 				jLabelCliente.setText("Cliente:");
-				jLabelCliente.setBounds(22, 137, 75, 38);
+				jLabelCliente.setBounds(33, 157, 75, 38);
 				jLabelCliente.setFont(new java.awt.Font("SansSerif", 1, 12));
+			}
+			{
+				jLabelPeriodicidad = new JLabel();
+				this.add(jLabelPeriodicidad);
+				jLabelPeriodicidad.setText("Periodicidad:");
+				jLabelPeriodicidad.setBounds(33, 103, 75, 38);
+				jLabelPeriodicidad.setFont(new java.awt.Font("SansSerif", 1, 12));
 			}
 			{
 				jLabelPedidosPendientes = new JLabel();
 				this.add(jLabelPedidosPendientes);
 				jLabelPedidosPendientes.setText("1) Seleccionar un pedido a programar:");
-				jLabelPedidosPendientes.setBounds(33, 22, 248, 19);
+				jLabelPedidosPendientes.setBounds(39, 16, 248, 19);
 				jLabelPedidosPendientes.setFont(new java.awt.Font("Segoe UI", 1, 13));
 			}
 			{
-				comboProgramados = new JComboBox<String>();
-				this.add(comboProgramados);
-				comboProgramados.setBounds(26, 49, 254, 26);
+				comboPendientes = new JComboBox<String>();
+				this.add(comboPendientes);
+				comboPendientes.setBounds(37, 40, 254, 26);
 
 				pedidos = Sistema.getInstancia().getPedidos("pendiente");
 				for (int i = 0; i < pedidos.size(); i++)
-					comboProgramados.addItem(pedidos.get(i).getDescripcion());
+					comboPendientes.addItem(pedidos.get(i).getDescripcion());
 
-				comboProgramados.addActionListener(new ActionListener() {
-					@Override
+				comboPendientes.addActionListener(new ActionListener() {
+
 					public void actionPerformed(ActionEvent evt) {
 						for (int i = 0; i < pedidos.size(); i++)
-							if (comboProgramados.getSelectedItem().toString().equals(pedidos.get(i).getDescripcion()))
+							if (comboPendientes.getSelectedItem().toString().equals(pedidos.get(i).getDescripcion()))
 								pedido = pedidos.get(i);
 
 						jTextFieldId.setText(pedido.getId() + "");
 						jTextFieldFechaSolicitud.setText(pedido.getFechaSolicitud() + "");
 						jTextFieldCliente.setText(pedido.getCliente().getNombre());
+						if (pedido.getPeriodicidad() == 0)
+							jTextFieldPeriodicidad.setText("No");
+						else
+							jTextFieldPeriodicidad.setText(pedido.getPeriodicidad() + "");
 					}
 				});
 
@@ -135,7 +132,7 @@ public class PedidoProgramar extends javax.swing.JPanel {
 				jLabelFechaSolicitud = new JLabel();
 				this.add(jLabelFechaSolicitud);
 				jLabelFechaSolicitud.setText("Fecha de solicitud:");
-				jLabelFechaSolicitud.setBounds(22, 111, 129, 38);
+				jLabelFechaSolicitud.setBounds(33, 131, 129, 38);
 				jLabelFechaSolicitud.setFont(new java.awt.Font("SansSerif", 1, 12));
 			}
 
@@ -143,13 +140,13 @@ public class PedidoProgramar extends javax.swing.JPanel {
 				jLabelId = new JLabel();
 				this.add(jLabelId);
 				jLabelId.setText("Id pedido:");
-				jLabelId.setBounds(24, 83, 75, 38);
+				jLabelId.setBounds(33, 75, 75, 38);
 				jLabelId.setFont(new java.awt.Font("SansSerif", 1, 12));
 			}
 			{
 				jTextFieldCliente = new JTextField();
 				this.add(jTextFieldCliente);
-				jTextFieldCliente.setBounds(141, 146, 163, 21);
+				jTextFieldCliente.setBounds(149, 163, 163, 28);
 				jTextFieldCliente.setEnabled(false);
 				jTextFieldCliente.setEditable(false);
 			}
@@ -171,28 +168,35 @@ public class PedidoProgramar extends javax.swing.JPanel {
 				jLabel5 = new JLabel();
 				this.add(jLabel5);
 				jLabel5.setText("4) Ingresar fecha de inicio:");
-				jLabel5.setBounds(260, 211, 163, 16);
+				jLabel5.setBounds(260, 226, 163, 16);
 				jLabel5.setFont(new java.awt.Font("SansSerif", 1, 12));
 			}
 
 			{
 				jTextFieldFechaSolicitud = new JTextField();
 				this.add(jTextFieldFechaSolicitud);
-				jTextFieldFechaSolicitud.setBounds(141, 119, 163, 21);
+				jTextFieldFechaSolicitud.setBounds(149, 136, 163, 28);
 				jTextFieldFechaSolicitud.setEditable(false);
 				jTextFieldFechaSolicitud.setEnabled(false);
 			}
 			{
 				jTextFieldId = new JTextField();
 				this.add(jTextFieldId);
-				jTextFieldId.setBounds(141, 90, 163, 21);
+				jTextFieldId.setBounds(149, 82, 163, 28);
 				jTextFieldId.setEditable(false);
 				jTextFieldId.setEnabled(false);
 			}
 			{
+				jTextFieldPeriodicidad = new JTextField();
+				this.add(jTextFieldPeriodicidad);
+				jTextFieldPeriodicidad.setBounds(149, 109, 163, 28);
+				jTextFieldPeriodicidad.setEditable(false);
+				jTextFieldPeriodicidad.setEnabled(false);
+			}
+			{
 				jTextFieldFecha = new JTextField();
 				this.add(jTextFieldFecha);
-				jTextFieldFecha.setBounds(251, 238, 179, 27);
+				jTextFieldFecha.setBounds(251, 249, 179, 27);
 				jTextFieldFecha.setEnabled(true);
 				jTextFieldFecha.setText("dd/mm/aaaa");
 			}
@@ -205,7 +209,7 @@ public class PedidoProgramar extends javax.swing.JPanel {
 			{
 				jSeparator2 = new JSeparator();
 				this.add(jSeparator2);
-				jSeparator2.setBounds(-1, 188, 700, 11);
+				jSeparator2.setBounds(-1, 207, 700, 11);
 			}
 
 			{
@@ -220,10 +224,7 @@ public class PedidoProgramar extends javax.swing.JPanel {
 				}
 
 				comboTipo.addActionListener(new ActionListener() {
-					@Override
 					public void actionPerformed(ActionEvent evt) {
-						System.out.println("comboTipo.actionPerformed, event=" + evt);
-
 						for (int i = 0; i < tipos.size(); i++)
 							if (comboTipo.getSelectedItem().toString().equals(tipos.get(i).getDescripcion()))
 								tipo = tipos.get(i);
@@ -234,7 +235,7 @@ public class PedidoProgramar extends javax.swing.JPanel {
 
 			{
 				comboComplejidad = new JComboBox<String>();
-				getContentPane().add(comboComplejidad);
+				this.add(comboComplejidad);
 				comboComplejidad.setBounds(418, 123, 216, 26);
 
 				complejidades = Sistema.getInstancia().getComplejidadesPedido();
@@ -244,43 +245,14 @@ public class PedidoProgramar extends javax.swing.JPanel {
 				}
 
 				comboComplejidad.addActionListener(new ActionListener() {
-					@Override
 					public void actionPerformed(ActionEvent evt) {
-						System.out.println("comboComplejidad.actionPerformed, event=" + evt);
 						for (int i = 0; i < tipos.size(); i++)
 							if (comboComplejidad.getSelectedItem().toString().equals(complejidades.get(i).getNombre()))
 								complejidad = complejidades.get(i);
-						jButtonFinalizarPedido.setEnabled(true);
-						// Fecha Desde
-						DateTime solicitud = new DateTime(new Date());
-						jTextFieldFechapedidoDesde.setText(Integer.parseInt(solicitud.toString("dd")) + "/" + Integer.parseInt(solicitud.toString("MM")) + "/"
-								+ Integer.parseInt(solicitud.toString("YYYY")));
 
-						// Fecha Hasta
-						// duracion en dias
-						float d = (tipo.getTiempo() * complejidad.getFactorTiempo());
-						int dur = 0;
-						if (d > (int) d) {
-							dur = (int) d + 1;
-						} else {
-							dur = (int) d;
-						}
-
-						String solicitud1 = null;
-						try {
-							solicitud1 = Sistema.getInstancia().sumarRestarDiasFecha(pedido.getFechaEntrega(), -dur);
-						} catch (ParseException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						jTextFieldFechaPedidoHasta.setText(solicitud1);
-
-						jTextFieldFechaPedidoHasta.setEnabled(false);
-						jTextFieldFechapedidoDesde.setEnabled(false);
 					}
 
 				});
-				complejidad = complejidades.get(0);
 
 			}
 
@@ -289,28 +261,29 @@ public class PedidoProgramar extends javax.swing.JPanel {
 				jButtonConfirmar.setLayout(null);
 				this.add(jButtonConfirmar);
 				jButtonConfirmar.setText("Confirmar");
-				jButtonConfirmar.setBounds(183, 304, 91, 40);
+				jButtonConfirmar.setBounds(181, 304, 91, 40);
 				jButtonConfirmar.setFont(new java.awt.Font("SansSerif", 1, 13));
 				jButtonConfirmar.addActionListener(new ActionListener() {
-					@Override
+
 					public void actionPerformed(ActionEvent evt) {
-						System.out.println("jButtonFinalizarPedido.actionPerformed, event=" + evt);
+
 						try {
-							Sistema.getInstancia().finalizarPedido(pedido);
+							Sistema.getInstancia().programarPedido(pedido, tipo, complejidad);
 						} catch (ParseException e) {
 							e.printStackTrace();
 						}
+
 						jTextFieldId.setText("");
 						jTextFieldFechaSolicitud.setText("");
 						jTextFieldCliente.setText("");
+						jTextFieldPeriodicidad.setText("");
 
 						pedidos.clear();
-						comboProgramados.removeAllItems();
+						comboPendientes.removeAllItems();
 						pedidos = Sistema.getInstancia().getPedidos("pendiente");
 						for (int i = 0; i < clientes.size(); i++)
-							comboProgramados.addItem(pedidos.get(i).getDescripcion());
+							comboPendientes.addItem(pedidos.get(i).getDescripcion());
 
-						JOptionPane.showMessageDialog(null, "Pedido finalizado.");
 					}
 				});
 			}
@@ -321,7 +294,7 @@ public class PedidoProgramar extends javax.swing.JPanel {
 				jButtonSalir.setBounds(396, 304, 91, 40);
 				jButtonSalir.setFont(new java.awt.Font("SansSerif", 1, 13));
 				jButtonSalir.addActionListener(new ActionListener() {
-					@Override
+
 					public void actionPerformed(ActionEvent e) {
 						panel.remove(instancia);
 					}
